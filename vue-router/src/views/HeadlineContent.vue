fa317690ed12b9d2046b50016e0bcb264fc9e96b
Yi-Chin Weng <yichinweng@gmail.com>
2018-11-14 21:15:11 +0800

    Fix bug, data might be not ready when key in post url directly.

--- vue-router/src/views/HeadlineContent.vue
+++ vue-router/src/views/HeadlineContent.vue
@@ -1,22 +1,44 @@
 <template>
   <div id="headline-content">
-    <h1>{{ article.title }}</h1>
-    <div class="content">
-      {{ article.content }}
+    <p v-if="fetching">fetching...</p>
+    <div v-if="article">
+      <h1>{{ article.title }}</h1>
+      <div class="content">
+        {{ article.content }}
+      </div>
+      <div v-if="article.urlToImage"><img v-bind:src="article.urlToImage"></div>
+      <a v-bind:href="article.url" target="_blank">More...</a>
     </div>
-    <div v-if="article.urlToImage"><img v-bind:src="article.urlToImage"></div>
-    <a v-bind:href="article.url" target="_blank">More...</a>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'headlinecontent',
   computed: {
+    fetching() {
+      return this.$store.state.fetching
+    },
     article() {
+      if (this.$store.state.headlines.status !== 'ok') {
+        return false;
+      }
       return this.$store.state.headlines.articles[this.$route.params.id]
     }
-  }
+  },
+  methods: {
+    ...mapActions([
+      'getHeadlines',
+    ])
+  },
+  mounted() {
+    if (this.$store.state.headlines.status === 'ok') {
+      return;
+    }
+    this.getHeadlines();
+  },
 }
 </script>
 
