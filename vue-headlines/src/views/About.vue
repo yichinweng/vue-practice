5e46abc78139bfc61c1e6e7814beb97b04b5a8d4
Winter Weng <yichinweng@gmail.com>
2018-11-15 10:41:42 +0800

    Accessing child component instances & child elements.
    https://vuejs.org/v2/guide/components-edge-cases.html#Accessing-Child-Component-Instances-amp-Child-Elements

--- vue-headlines/src/views/About.vue
+++ vue-headlines/src/views/About.vue
@@ -10,6 +10,10 @@
       </template>
       Hi, I'm Winter. {{ msg }}
     </SlotTest>
+    <base-input ref="usernameInput"></base-input>
+    <div>
+      <button v-on:click="focusInput()">Focus input box</button>
+    </div>
   </div>
 </template>
 
@@ -28,13 +32,32 @@ const SlotTest = {
   `
 };
 
+const baseInput = {
+  template: `
+    <p>
+      <input ref="input">
+    </p>
+  `,
+  methods: {
+    focus: function() {
+      this.$refs.input.focus()
+    }
+  }
+}
+
 export default {
   components: {
     SlotTest,
+    baseInput
   },
   computed: {
     msg() {
       return this.$store.state.msg
+    }
+  },
+  methods: {
+    focusInput: function() {
+      this.$refs.usernameInput.focus()
     }
   }
 }
