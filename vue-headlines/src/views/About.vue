b45301cad51f88a7aaf57c216b65c1a5444fe177
Winter Weng <yichinweng@gmail.com>
2018-11-15 10:27:37 +0800

    Slot using.

--- vue-headlines/src/views/About.vue
+++ vue-headlines/src/views/About.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="about">
     <h1>{{ msg }}</h1>
+    <SlotTest>
+      <template slot="title">
+        This is a slot test.
+      </template>
+      <template slot="content">
+        first multi-slot test.
+      </template>
+      Hi, I'm Winter. {{ msg }}
+    </SlotTest>
   </div>
 </template>
 
 <script>
+const SlotTest = {
+  template: `
+    <div>
+      <h2>
+        <slot name="title"></slot>
+      </h2>
+      <p>
+        <slot name="content"></slot>
+      </p>
+      <slot></slot>
+    </div>
+  `
+};
+
 export default {
+  components: {
+    SlotTest,
+  },
   computed: {
     msg() {
       return this.$store.state.msg
